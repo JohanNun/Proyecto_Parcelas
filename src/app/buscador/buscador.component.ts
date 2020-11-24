638572823d69d38b959481891a4fb6bb3ddc7ff6
JohanNunez <jo.nunez@hotmail.com>
2020-11-24 21:17:29 +0100

    Creación componente pagina-busqueda + arreglo css

--- src/app/buscador/buscador.component.ts
+++ src/app/buscador/buscador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-buscador',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  onBusqueda(pRuta: string) {
+    this.router.navigate([pRuta]);
   }
 
 }
