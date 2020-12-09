9bc91ae3109cb734b987b153dd330b74ffd4e75a
JohanNunez <jo.nunez@hotmail.com>
2020-12-09 17:40:09 +0100

    los putos mapas

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,7 +1,6 @@
-
-
-
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HuertComService } from '../services/huert-com.service';
 
 @Component({
   selector: 'app-mapa',
@@ -10,19 +9,62 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  huertosLocalizacion: any[];
+  huertos2: any;
+
   @Input() latitud: number;
   @Input() longitud: number;
   @Input() zoom: number;
   @Input() buscador: boolean = true;
 
-  constructor() { }
+  constructor(private huertComService: HuertComService,
+    private activatedRoute: ActivatedRoute) {
+    this.huertosLocalizacion = [];
+  }
 
   ngOnInit() {
     navigator.geolocation.getCurrentPosition(position => {
+
       this.latitud = position.coords.latitude;
       this.longitud = position.coords.longitude;
-      this.zoom = 13;
+      /* this.zoom = 1; */
     });
+
+    /* Consulta bd y traer todos los huertos */
+
+
+    let id = "";
+    id = this.activatedRoute.snapshot.params['huertoId'];
+
+    console.log(id);
+
+    if (id === undefined) {
+      this.huertComService.getAll()
+        .then(result => {
+          for (let huerto of result) {
+
+            this.huertosLocalizacion.push(
+              {
+                latitud: huerto.latitud,
+                longitud: huerto.longitud,
+                zoom: 8
+              })
+          }
+        })
+    } else {
+      this.huertComService.getById(id)
+        .then(result => {
+
+          this.huertosLocalizacion.push({
+            latitud: result.latitud,
+            longitud: result.longitud,
+            zoom: 17
+          });
+
+
+        })
+    }
+
   }
 
 }
