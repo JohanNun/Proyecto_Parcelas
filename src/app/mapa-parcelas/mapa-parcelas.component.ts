d21d04e9640f1189158dc491f728dc0a1d342612
JohanNunez <jo.nunez@hotmail.com>
2020-12-09 18:11:54 +0100

    mapas parcelas

---
+++ src/app/mapa-parcelas/mapa-parcelas.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { parcela, ParcelasService } from '../services/parcelas.service';
+
+@Component({
+  selector: 'app-mapa-parcelas',
+  templateUrl: './mapa-parcelas.component.html',
+  styleUrls: ['./mapa-parcelas.component.css']
+})
+export class MapaParcelasComponent implements OnInit {
+
+  @Input() latitud: number;
+  @Input() longitud: number;
+  @Input() zoom: number;
+  @Input() buscador: boolean = true;
+
+  parcelitas: parcela[];
+  parcelas: any[];
+
+  constructor(private parcelasService: ParcelasService,
+    private activatedRoute: ActivatedRoute) {
+    this.parcelas = [];
+  }
+
+  ngOnInit(): void {
+
+    navigator.geolocation.getCurrentPosition(position => {
+
+      this.latitud = position.coords.latitude;
+      this.longitud = position.coords.longitude;
+
+    });
+
+
+
+    this.parcelasService.getAll()
+      .then(result => {
+        this.parcelitas = result
+        console.log(this.parcelitas);
+
+
+        const positionParcelas = new Array();
+
+        for (let parcela of this.parcelitas) {
+
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({ address: parcela.localizacion }, function (result, status) {
+
+            if (status === google.maps.GeocoderStatus.OK) {
+              let position = result[0].geometry.location;
+              positionParcelas.push({ latitud: position.lat(), longitud: position.lng() });
+
+            }
+
+          });
+        }
+
+        this.parcelas = positionParcelas;
+        console.log(this.parcelas);
+
+
+
+
+      })
+      .catch(error => console.log(error))
+
+  }
+
+}
--- src/app/pagina-busqueda/pagina-busqueda.component.ts
+++ src/app/pagina-busqueda/pagina-busqueda.component.ts
@@ -49,39 +49,37 @@ export class PaginaBusquedaComponent implements OnInit {
         this.parcelitas = result
         console.log(this.parcelitas);
 
-
-        const positionParcelas = new Array();
-
-        for (let parcela of this.parcelitas) {
-
-
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode({ address: parcela.localizacion }, function (result, status) {
-
-            if (status === google.maps.GeocoderStatus.OK) {
-              let position = result[0].geometry.location;
-              positionParcelas.push({ latitud: position.lat(), longitud: position.lng() });
-
-            }
-
-          });
-        }
-
-        this.parcelas = positionParcelas;
-        console.log(this.parcelas);
-
-
-
-
       })
-      .catch(error => console.log(error))
-
-
-
-
-
-
   }
+  /*  const positionParcelas = new Array();
+ 
+   for (let parcela of this.parcelitas) {
+ 
+ 
+     var geocoder = new google.maps.Geocoder();
+     geocoder.geocode({ address: parcela.localizacion }, function (result, status) {
+ 
+       if (status === google.maps.GeocoderStatus.OK) {
+         let position = result[0].geometry.location;
+         positionParcelas.push({ latitud: position.lat(), longitud: position.lng() });
+ 
+       }
+ 
+     });
+   }
+ 
+   this.parcelas = positionParcelas;
+   console.log(this.parcelas);
+ 
+ 
+ 
+ 
+ })
+ .catch(error => console.log(error))
+*/
+
+
+
 
 
 
